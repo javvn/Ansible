65f2ba7b28c9abd8d65e45bcf988f4b38c843a06
javvn <juho0428@naver.com>
2023-01-02 17:32:56 +0900

    init sg

--- ec2-instance/_local.tf
+++ ec2-instance/_local.tf
@@ -6,6 +6,7 @@ locals {
   remote_states = {
     "network" = data.terraform_remote_state.this["network"].outputs
   }
+  name                  = local.remote_states["network"].name
   vpc                   = local.remote_states["network"].vpc_id
   private_subnet_groups = local.remote_states["network"].private_subnets
   public_subnet_groups  = local.remote_states["network"].public_subnets
